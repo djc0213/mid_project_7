215d49f6b9c999c23e135c4c13caadb7041fa981
chul6 <hyungchul0604@gmail.com>
2023-04-26 19:51:11 +0900

    NavigationBar 추가

--- lib/main.dart
+++ lib/main.dart
@@ -26,34 +26,33 @@ class MainPage extends StatefulWidget {
   MainPageState createState() => MainPageState();
 }
 
+class MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
 
-  const MyFilesPage({super.key});
+  static final List<Widget> _pageOptions = [
+    const FeedPage(),
+    const FriendListPage(),
+    const MyFilesPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('친구 게시물'),
-      ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemCount: friendPosts.length,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                image: NetworkImage(friendPosts[index]),
-                fit: BoxFit.cover,
-              ),
-            ),
-          );
-        },
+      body: _pageOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.feed), label: 'Feed'),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Friends'),
+          BottomNavigationBarItem(icon: Icon(Icons.folder), label: 'My Files'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
