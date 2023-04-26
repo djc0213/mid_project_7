3958e580263cee6bb87e087c12922cc4b61f0a24
chul6 <hyungchul0604@gmail.com>
2023-04-26 22:19:14 +0900

    Jakob#4 개선 (앱 전체 테마 색 정의하며 일관성 부여)

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Social Media App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.teal,
+          titleTextStyle: TextStyle(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
       ),
       home: const MainPage(),
     );
